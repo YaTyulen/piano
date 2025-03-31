b7f27fe6ba8e543fad69d9a6e5b7cabd1a682783
Yana <yana@CPS10-MB-001.gazpromcps.ru>
2025-03-31 16:01:05 +0700

    исправляет не срабатывание ноты при клике по клавише

--- script.js
+++ script.js
@@ -5,34 +5,6 @@ let switchCheckbox = document.querySelector('.piano-help-switch')
 let keyList = document.querySelectorAll('.piano-key') // Получаем список клавиш
 
 keyList.forEach((key, index) => {
-    //добавляем обработчик по кнопке клавиатуры этой клавише
-    key.addEventListener('keydown', (event) => {
-        if(event.code == 'KeyZ' && index === 1) playNote(index)
-        if(event.code == 'KeyX' && index === 2) playNote(index)
-        if(event.code == 'KeyC' && index === 3) playNote(index)
-        if(event.code == 'KeyV' && index === 4) playNote(index)
-        if(event.code == 'KeyB' && index === 5) playNote(index)
-        if(event.code == 'KeyN' && index === 6) playNote(index)
-        if(event.code == 'KeyM' && index === 7) playNote(index)
-        if(event.code == 'KeyA' && index === 8) playNote(index)
-        if(event.code == 'KeyW' && index === 9) playNote(index)
-        if(event.code == 'KeyS' && index === 10) playNote(index)
-        if(event.code == 'KeyE' && index === 11) playNote(index)
-        if(event.code == 'KeyD' && index === 12) playNote(index)
-        if(event.code == 'KeyF' && index === 13) playNote(index)
-        if(event.code == 'KeyT' && index === 14) playNote(index)
-        if(event.code == 'KeyG' && index === 15) playNote(index)
-        if(event.code == 'KeyY' && index === 16) playNote(index)
-        if(event.code == 'KeyH' && index === 17) playNote(index)
-        if(event.code == 'KeyU' && index === 18) playNote(index)
-        if(event.code == 'KeyJ' && index === 19) playNote(index)
-        if(event.code == 'KeyK' && index === 20) playNote(index)
-        if(event.code == 'KeyO' && index === 21) playNote(index)
-        if(event.code == 'KeyL' && index === 22) playNote(index)
-        if(event.code == 'KeyP' && index === 23) playNote(index)
-        if(event.code == 'Key;' && index === 24) playNote(index)
-    })
-
     // итерируемся по списку клавиш и переводим index клавиши в строку
     index = String(index + 1).length === 1 ? '0'+ (index + 1) : index + 1;
 
@@ -47,6 +19,34 @@ keyList.forEach((key, index) => {
         event.stopPropagation(); 
         playNote(index)
     })
+})
+
+//добавляем обработчик по кнопке клавиатуры
+document.addEventListener('keydown', (event) => {
+    if(event.code == 'KeyZ') playNote('01')
+    if(event.code == 'KeyX') playNote('02')
+    if(event.code == 'KeyC') playNote('03')
+    if(event.code == 'KeyV') playNote('04')
+    if(event.code == 'KeyB') playNote('05')
+    if(event.code == 'KeyN') playNote('06')
+    if(event.code == 'KeyM') playNote('07')
+    if(event.code == 'KeyA') playNote('08')
+    if(event.code == 'KeyW') playNote('09')
+    if(event.code == 'KeyS') playNote('10')
+    if(event.code == 'KeyE') playNote('11')
+    if(event.code == 'KeyD') playNote('12')
+    if(event.code == 'KeyF') playNote('13')
+    if(event.code == 'KeyT') playNote('14')
+    if(event.code == 'KeyG') playNote('15')
+    if(event.code == 'KeyY') playNote('16')
+    if(event.code == 'KeyH') playNote('17')
+    if(event.code == 'KeyU') playNote('18')
+    if(event.code == 'KeyJ') playNote('19')
+    if(event.code == 'KeyK') playNote('20')
+    if(event.code == 'KeyO') playNote('21')
+    if(event.code == 'KeyL') playNote('22')
+    if(event.code == 'KeyP') playNote('23')
+    if(event.code == 'Key;') playNote('24')
 })
 
 // Функция воспроизведения звука, в засисимости от index клавиши
